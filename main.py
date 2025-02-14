27bb3575e69e24a955e87ff870e99f1048444c74
bobbyshop-vui <info@bobby-shop.com>
2025-02-14 21:20:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import streamlit as st
-import time
+st.title(':collision: ĐIỀN THÔNG TIN GIỚI THIỆU BẢN THÂN')
+my_bar = st.progress(0)
+quiz = ["Họ và tên:", "Ngày sinh:", "Nơi sinh","Sở thích:"]
+answers = []
+len_quiz = len(quiz)
+for i in range(len_quiz):
+  tl = st.text_input(quiz[i])
+  if tl !="":
+    answers.append(tl)
 
-st.title('my progress')
-
-bar = st.progress(0)
-if st.button('Nhấn'):
+if st.button("Confirm"):
+  if len(answers)==len_quiz:
+    my_bar.progress(100)
+    st.write("Bạn đã điền đầy đủ thông tin")
     st.balloons()
-for progress_completed in range(100):
-    time.sleep(0.5)
-    bar.progress(progress_completed + 1)
+  else:
+    my_bar.progress(len(answers)/len_quiz)
+    st.write("Bạn chưa điền đủ thông tin")
+
+for i in range (len(answers)):
+  st.write(quiz[i],answers[i])
